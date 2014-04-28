a634700cf3b1e9f82984cb258209c49d4b04f899
Difrex(Denis Zheleztsov) <dz@etegro.com>
2014-04-28 16:12:07 +0400

    Clipboard module

---
+++ Clip.pm
@@ -0,0 +1,28 @@
+package Clip;
+
+use Clipboard;
+
+sub new {
+    my $class = shift;
+
+    my $self = { _password => shift, };
+
+    bless $self, $class;
+    return $self;
+}
+
+sub copy {
+    my ($self) = @_;
+    my $password = $self->{_password};
+
+    if ( 'Clipboard::Xclip' eq $Clipboard::driver ) {
+        no warnings 'redefine';
+        *Clipboard::Xclip::all_selections = sub {
+            qw(clipboard primary buffer secondary);
+        };
+    }
+
+    Clipboard->copy("$password");
+}
+
+1;
