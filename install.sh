fce94c1e44455c0215b8dc5b49bd1941c4fabe67
Difrex <difrex.punk@gmail.com>
2015-01-20 12:03:34 +0300

    Installation script

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+PWD=`pwd`
+BIN_PATH=${HOME}/.local/bin
+
+mkdir -p $BIN_PATH
+
+cat > $BIN_PATH/pm << EOF
+cd $PWD
+./pm.pl "\$@"
+cd - >/dev/null 2>/dev/null
+EOF
+
+chmod +x $BIN_PATH/pm
+
+echo "Please add $BIN_PATH to your \$PATH variable"
